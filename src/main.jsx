6ff1002321eb88f6877153d135be10d0f259f2b5
rizkynopen <rizkynofendric5@gmail.com>
2026-04-13 12:47:57 +0700

    final 5

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/react---quran-API/">
-    <App />
-  </BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
